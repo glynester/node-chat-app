6984052678ff8a0a8efe43679662adf22382f0c9
Gee <glynester@hotmail.com>
2022-01-27 22:24:09 +0000

    More explanatory notes added to counter mini app.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,11 @@
-//Store return value in a variable.
+// Store return value in a variable.
 // Will be able to send and receive events from the server and the client.
 const socket = io();     // Fn is available because of this script being loaded on index.html - "/socket.io/socket.io.js"
 
-
 // receive event
 socket.on('countUpdated', (count)=>{
-  // 2nd argument on index.js (socket.emit) is first argument here
+  // 2nd argument on index.js (socket.emit) is first argument here.
   console.log('The count has been updated ', count);
-
 })
 
 document.querySelector('#increment').addEventListener('click',()=>{
--- src/index.js
+++ src/index.js
@@ -4,29 +4,30 @@ const express=require('express');
 const socketio=require('socket.io');
 
 const app=express();
-const server = http.createServer(app); // creates new webserver. Orinarily this is automatically done but we specifically need to access this server variable
+const server = http.createServer(app); // creates new webserver. Ordinarily this is automatically done but we specifically need to access this server variable for setting up socketio.
 // socket io must be called with raw http server.
 const io=socketio(server); // create new instance of socket.io to configure websockets to work with our server
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 const port=process.env.PORT || 3000;
-app.use(express.static(publicDirectoryPath));   // Serves up contents of this 'public' folder
+app.use(express.static(publicDirectoryPath));   // SERVES up contents of this 'public' folder so we can access any file in this folder, e.g. test.html is accessible from localhost:3000/test.html (index.html is available from just 'localhost:3000/' by default)
 
-// server (emit) --> client(receive) - countUpdated
-// client (emit) --> server(receive) - increment
+// server (emit) --> client(receive) - 'countUpdated'
+// client (emit) --> server(receive) - 'increment'
+// The custom event names (countUpdated + increment) must be the same on index.js as they are on chat.js
 
 let count=0;
-// listening for a given event to occur - "connection" and fn to run when that event occurs.
+// listening for a given event to occur - "connection" (std event - not custom) and fn to run when that event occurs.
 // io.on('connection',()=>{ // socket is an object that contains info about the new connection. We can use methods on this to communicate with that client.
   io.on('connection',(socket)=>{
   console.log('New WebSocket connection');  // Sends msg to terminal wehn given client connects
-  // 2nd argument is available on client (chat.js) callback.
+  // 2nd argument ('count' here) is available on client (chat.js) callback.
   // No need to send this to all clients - just one who has connected
-  socket.emit('countUpdated', count);  // Sending and receiving events. Send (custom) event to client.
+  socket.emit('countUpdated', count);  // Sending and receiving events. Send (custom) event to client who has just onnected.
   socket.on('increment',()=>{
-    count++;
-    // socket.emit('countUpdated', count); // Only emits to a single specific connection.
-    io.emit('countUpdated', count); // emits to EVERY connection!
+    count++;  // Just receiving the event is enough for us to increment the count - no reason to receive data also in this case.
+    // socket.emit('countUpdated', count); // 'socket.emit' only emits to a single specific connection.
+    io.emit('countUpdated', count); // 'io.emit' emits to EVERY connection! In this case when one user clicks, we want all users to see the updated count value.
   });
 })
 
