6870152ae1d613f9b6b440f5540a8cd3357f77be
Gee <glynester@hotmail.com>
2022-01-28 22:00:07 +0000

    Added form to allow message to be sent from any socket (client). Msg then sent to all sockets from server.

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,15 +2,20 @@
 // Will be able to send and receive events from the server and the client.
 const socket = io();     // Fn is available because of this script being loaded on index.html - "/socket.io/socket.io.js"
 
-// receive event
-socket.on('countUpdated', (count)=>{
-  // 2nd argument on index.js (socket.emit) is first argument here.
-  console.log('The count has been updated ', count);
+// Note - "message" event is received twice with different messages from server.
+socket.on('message',(message)=>{    // Receives from server.
+  console.log(message);
 })
 
-document.querySelector('#increment').addEventListener('click',()=>{
-  console.log('clicked!!!');
-  socket.emit('increment');   // emitted from client.
+// document.querySelector('form').addEventListener('submit',(e)=>{
+  document.querySelector('#message-form').addEventListener('submit',(e)=>{
+  e.preventDefault();   // Stops browser refreshing with form submit
+  // const message = document.querySelector('input').value;
+  // Another way to access the input value is from the "e" object.
+  // "e" has "target" property which is the target we're listening for the event on which is the form, so e.target in this case is the form.
+  // const message = e.target.message.value;  // This also works
+  const message = e.target.elements.message.value;
+  // console.log("Button clicked and msg is "+messageToServer);
+  socket.emit('sendMessage', message);    // Sends to server.
 });
-
 
--- src/index.js
+++ src/index.js
@@ -12,24 +12,15 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 const port=process.env.PORT || 3000;
 app.use(express.static(publicDirectoryPath));   // SERVES up contents of this 'public' folder so we can access any file in this folder, e.g. test.html is accessible from localhost:3000/test.html (index.html is available from just 'localhost:3000/' by default)
 
-// server (emit) --> client(receive) - 'countUpdated'
-// client (emit) --> server(receive) - 'increment'
-// The custom event names (countUpdated + increment) must be the same on index.js as they are on chat.js
-
-let count=0;
-// listening for a given event to occur - "connection" (std event - not custom) and fn to run when that event occurs.
-// io.on('connection',()=>{ // socket is an object that contains info about the new connection. We can use methods on this to communicate with that client.
-  io.on('connection',(socket)=>{
-  console.log('New WebSocket connection');  // Sends msg to terminal wehn given client connects
-  // 2nd argument ('count' here) is available on client (chat.js) callback.
-  // No need to send this to all clients - just one who has connected
-  socket.emit('countUpdated', count);  // Sending and receiving events. Send (custom) event to client who has just onnected.
-  socket.on('increment',()=>{
-    count++;  // Just receiving the event is enough for us to increment the count - no reason to receive data also in this case.
-    // socket.emit('countUpdated', count); // 'socket.emit' only emits to a single specific connection.
-    io.emit('countUpdated', count); // 'io.emit' emits to EVERY connection! In this case when one user clicks, we want all users to see the updated count value.
-  });
+io.on('connection',(socket)=>{
+  console.log("New Web Socket Connection!!!");
+  socket.emit('message',"Welcome to my realtime messaging app");   // Sends msg to terminal when given client connects
+  socket.on('sendMessage', (message)=>{ // When server receives a message from one socket, send it to all sockets (with io.emit)
+    console.log(message);
+    io.emit('message', message); // Sent to all sockets.
+  })
 })
+// Note - "message" event is emitted twice from server (see above) with different messages.
 
 // app.listen(port,()=>{
 server.listen(port,()=>{
